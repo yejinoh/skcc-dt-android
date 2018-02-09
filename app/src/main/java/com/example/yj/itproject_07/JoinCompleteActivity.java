8d89dc5f7d3ff34a9728ee052d39aafb9debd62a
jutopiha <migrate1107@gmail.com>
2018-02-09 22:46:15 +0900

    가입 완료 화면

---
+++ app/src/main/java/com/example/yj/itproject_07/JoinCompleteActivity.java
@@ -0,0 +1,22 @@
+package com.example.yj.itproject_07;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+/**
+ * Created by Administrator on 2018-02-09.
+ */
+public class JoinCompleteActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_join);
+
+       //5초 세고 끄기
+    }
+
+}

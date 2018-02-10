4d6f30667351f90aa181fd2de58eb573d55c2364
Juha2 <migrate1107@gmail.com>
2018-02-10 17:34:03 +0900

    수정

--- app/src/main/java/com/example/yj/itproject_07/DetailActivity.java
+++ app/src/main/java/com/example/yj/itproject_07/DetailActivity.java
@@ -5,6 +5,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -12,10 +13,30 @@ import android.widget.Toast;
  */
 
 public class DetailActivity extends AppCompatActivity{
+    public static String name,company,storage,color,price;
+
+//    textViewPhoneNameReal
+  //          textViewPhoneCompanyReal
+    //textViewPhoneStorageReal
+      //      textViewPhoneColorReal
+    //textViewPhonePriceReal
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_detail);
+
+        TextView nameview = findViewById(R.id.textViewPhoneNameReal);
+        TextView storageview = findViewById(R.id.textViewPhoneStorageReal);
+        TextView companyview = findViewById(R.id.textViewPhoneComapanyReal);
+        TextView colorview = findViewById(R.id.textViewPhoneColorReal);
+        TextView priceview = findViewById(R.id.textViewPhonePriceReal);
+
+        nameview.setText(name);
+        storageview.setText(storage);
+        companyview.setText(company);
+        colorview.setText(color);
+        priceview.setText(price);
 
         ImageView buttonExit = (ImageView)findViewById(R.id.buttonExit);
         buttonExit.setOnClickListener(new View.OnClickListener(){
